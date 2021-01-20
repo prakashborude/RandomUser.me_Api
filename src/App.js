fb1bdbe27eec636a883632b16f577323d3d1709a
prakashborude <borudepbsmart@gmail.com>
2021-01-21 01:03:59 +0530

    commit1

--- src/App.js
+++ src/App.js
@@ -1,25 +1,46 @@
-import logo from './logo.svg';
-import './App.css';
+import React ,{Component} from 'react';
+class App extends Component{
+  constructor(props)
+  {
+   super(props)
+   this.state={
+     item:[],
+     loading:false
+   } 
+  }
+  componentDidMount(){
+    fetch("https://randomuser.me/api/?results=10")
+    .then((response)=>response.json())
+    .then((response)=>{
+      this.setState(
+        {
+          item:response.results,
+          loading:true
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
-}
+        }
+      )
+    }
+    )
+  }
 
+  render(){
+    var{item,loading}=this.state
+    if(!loading)
+    {
+      return (<div>
+        Loading.....
+      </div>)
+
+    }
+    else{
+      return <div className="container">
+        {item.map(item=>(
+          <img src={item.picture.large}></img>
+          
+        ))}
+      </div>
+    }
+
+  }
+} 
 export default App;
